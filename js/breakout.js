90b47f6eb2352f0a599456460d01d71f6aec03c5
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-03-19 17:18:19 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,7 +1,23 @@
 rulesBtn = document.getElementById('rules-btn')
 rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
+canvas = document.getElementById('canvas')
+ctx = canvas.getContext('2d')
 
+// create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height /2,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+}
+
+// draw ball on canvas
+function drawBall() {
+    
+}
 
 // Rules open and clos event handlers
 rulesBtn.addEventListener('click', () => {
