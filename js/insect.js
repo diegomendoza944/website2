04668adcd3b6395037b38dd2a038fe46a511b8f2
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-04-10 16:16:07 +0000

    Wed, Apr 10, 2024, 9:16 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -33,4 +33,14 @@ choose_insect_btns.forEach(btn => {
 function createInsect() {
     const insect = document.createElement('div')
     insect.classList.add('insect')
+    const { x, y } = getRandomLocation()
+    insect.style.top = `${y}px`
+    insect.style.left = `${x}px`
+    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt"
 }
+    {
+        const width = window.innerWidth
+        const height = window.innerHeight
+        const x = Math.random() * (width - 200) + 100
+        const y = Math.random() * ()
+    }
