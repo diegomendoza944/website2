2a97e33a52526f8e264bcff96dc059bea32fa89f
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-04-10 16:09:39 +0000

    Wed, Apr 10, 2024, 9:09 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -24,5 +24,13 @@ choose_insect_btns.forEach(btn => {
         const src = img.getAttribute('src')
         screen[1].classList.add('up')
         selected_insect = {src, alt}
+        setTimeout(createInsect, 1000)
+        startGame()
     })
 })
+
+
+function createInsect() {
+    const insect = document.createElement('div')
+    insect.classList.add('insect')
+}
