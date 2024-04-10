f6f17d06ffe5ac46e5b5ef98505ae325aa43816a
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-04-10 16:18:46 +0000

    Wed, Apr 10, 2024, 9:18 AM -07:00

--- js/insect.js
+++ js/insect.js
@@ -36,7 +36,7 @@ function createInsect() {
     const { x, y } = getRandomLocation()
     insect.style.top = `${y}px`
     insect.style.left = `${x}px`
-    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt"
+    insect.innerHTML = `<img src="selected_insect.src" alt="selected_insect.alt" style = "transform: rotate(${Math.random() * 360}deg)
 }
     {
         const width = window.innerWidth
