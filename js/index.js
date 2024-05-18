a76e18f0338b02ede018169ba15e886d91b00878
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-05-18 04:33:31 +0000

    Fri, May 17, 2024, 9:33 PM -07:00

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+function changeText() {
+    const messageElement = document.getElementById('message');
+    messageElement.textContent = "The color is changed";
+
+    messageElement.style.backgroundColor = "#e0ffe0";
+
+    messageElement.style.color = "#008000";
+}
