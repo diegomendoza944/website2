4254b55e591a9996b505b082fac042fdf87f189f
diegomendoza944 <142750693+diegomendoza944@users.noreply.github.com>
2024-05-23 03:16:05 +0000

    Wed, May 22, 2024, 8:16 PM -07:00

--- js/hangman.js
+++ js/hangman.js
@@ -1,10 +1,18 @@
 const wordEl = document.getElementById('word')
+
 const wrongLettersEl = document.getElementById('wrong-letters')
+
 const playAgainBtn = document.getElementById('play-button')
+
 const popup = document.getElementById('popup-container')
+
 const notification = document.getElementById('notification-container')
+
 const finalMessage = document.getElementById('final-message')
+
+
 const figureParts = document.querySelectorAll('.figure-part')
+
 
 const word = ['pingpong', 'tennis', 'golf', 'wizard', 'basketball', 'football', 'soccer']
 
@@ -12,6 +20,8 @@ let selectedIndex = Math.floor(word.length * Math.random())
 let selectedWord = word[selectedIndex]
 
 const correctLetters = []
+
+
 const wrongLetters = []
 
 let gameRunning = true
@@ -20,15 +30,23 @@ function displayWord() {
     wordEl.innerHTML = `
     ${selectedWord.split('').map(letter => `
         <span class="letter">
+
+
             ${correctLetters.includes(letter) ? letter : ''}
+
+
         </span>
     `).join('')}
 `
 
     const innerWord = wordEl.innerText.replace(/\n/g, '')
     if (innerWord === selectedWord) {
+
+
         finalMessage.innerText = 'Congratulations! You won!'
         popup.style.display = 'flex'
+
+
         gameRunning = false
     }
 }
@@ -51,6 +69,8 @@ function updateWrongLettersEl() {
     if (wrongLetters.length === figureParts.length) {
         finalMessage.innerText = `You Lose!!\n The word was "${selectedWord}"`
         popup.style.display = 'flex'
+
+
         gameRunning = false
     }
 }
@@ -72,6 +92,8 @@ window.addEventListener('keydown', e => {
         if (selectedWord.includes(letter)) {
             if (!correctLetters.includes(letter)) {
                 correctLetters.push(letter)
+
+
                 displayWord()
             } else {
                 showNotification()
@@ -79,6 +101,8 @@ window.addEventListener('keydown', e => {
         } else {
             if (!wrongLetters.includes(letter)) {
                 wrongLetters.push(letter)
+
+
                 updateWrongLettersEl()
             } else {
                 showNotification()
@@ -97,8 +121,12 @@ playAgainBtn.addEventListener('click', () => {
     selectedWord = word[selectedIndex]
 
     displayWord()
+
+
     updateWrongLettersEl()
     popup.style.display = 'none'
+
+
     gameRunning = true
 })
 
